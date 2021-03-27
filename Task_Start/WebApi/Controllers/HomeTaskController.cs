fb17e9d3b7ea58b6047bee0029748245c0c0b1e2
Oleksandr Petryk <OleksandrPetryk@outlook.com>
2021-03-27 09:17:23 +0200

    Create home task Asp.Net Core WEB Api

---
+++ Task_Start/WebApi/Controllers/HomeTaskController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Dto;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeTaskController : Controller
+    {
+        private readonly HomeTaskService _homeTaskService;
+
+        public HomeTaskController(HomeTaskService homeTaskService)
+        {
+            _homeTaskService = homeTaskService;
+        }
+        // GET: api/HomeTask
+        [HttpGet]
+        public ActionResult<IEnumerable<HomeTaskDto>> Get()
+        {
+            return Ok(_homeTaskService.GetAllHomeTasks().Select(homeTask => HomeTaskDto.FromModel(homeTask)));
+        }
+
+        // GET api/HomeTask/id
+        [HttpGet("{id}")]
+        public ActionResult<HomeTaskDto> Get(int id)
+        {
+            var homeTask = _homeTaskService.GetHomeTaskById(id);
+
+            if (homeTask == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(HomeTaskDto.FromModel(homeTask));
+        }
+
+        // POST api/HomeTask/
+        [HttpPost]
+        public ActionResult Post([FromBody] HomeTaskDto value)
+        {
+            var creaeResult = _homeTaskService.CreateHomeTask(value.ToModel());
+            if (creaeResult.HasErrors)
+            {
+                return BadRequest(creaeResult.Errors);
+            }
+            return Ok(creaeResult);
+        }
+
+        // PUT api/HomeTask/id
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] HomeTaskDto value)
+        {
+            var updateResult = _homeTaskService.UpdateHomeTask(value.ToModel());
+            if (updateResult.HasErrors)
+            {
+                return BadRequest(updateResult.Errors);
+            }
+            return Accepted();
+        }
+
+        // DELETE api/HomeTask/id
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            _homeTaskService.DeleteHomeTask(id);
+            return Accepted();
+        }
+    }
+}
+
